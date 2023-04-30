39ef80deb1622c48e13b3e3a325d7f943d47495b
Code-Apprentice <codeguru42@gmail.com>
2023-04-30 16:51:37 -0600

    Ask for name

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,3 +1,10 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, what's your name?");
+    let mut name = String::new();
+    stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+    println!("Hello, {}", name);
 }
