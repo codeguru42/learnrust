48cfdfc0f98dc9910ceadcd482df00ceaab5d899
Code-Apprentice <codeguru42@gmail.com>
2023-05-02 17:52:22 -0600

    Render map

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
@@ -20,6 +22,18 @@ impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    TileType::Wall => ctx.set(x, y, YELLOW, BLACK, to_cp437('#'));
+                }
+            }
         }
     }
 }
