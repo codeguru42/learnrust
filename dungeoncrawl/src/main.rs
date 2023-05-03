5707ce3acfb1539fb8fdd7147ac4764db47db27a
Code-Apprentice <codeguru42@gmail.com>
2023-05-02 18:52:57 -0600

    player module

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,11 +1,13 @@
 use prelude::*;
 
 mod map;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
 
     pub use crate::map::*;
+    pub use crate::player::*;
 
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
