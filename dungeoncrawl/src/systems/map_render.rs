84b89a79182f78343f81cfee05241d75499422de
Code-Apprentice <codeguru42@gmail.com>
2023-05-14 11:41:24 -0600

    Fix one more typo

--- dungeoncrawl/src/systems/map_render.rs
+++ dungeoncrawl/src/systems/map_render.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 
 #[system]
-pub fn map_render(#[resource] map: &Map, #[resource] camera: Camera) {
+pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
     for y in camera.top_y..=camera.bottom_y {
