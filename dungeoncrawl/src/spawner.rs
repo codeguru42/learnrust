cbbda32c9628879b97464067c57761bafd04dac0
Code-Apprentice <codeguru42@gmail.com>
2023-06-13 19:23:21 -0600

    Add field of view to player and monsters

--- dungeoncrawl/src/spawner.rs
+++ dungeoncrawl/src/spawner.rs
@@ -11,7 +11,8 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
         Health {
             current: 10,
             max: 10,
-        }
+        },
+        FieldOfView::new(8),
     ));
 }
 
@@ -31,7 +32,8 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
             },
             ChasingPlayer{},
             Health { current: hp, max: hp },
-            Name(name)
+            Name(name),
+            FieldOfView::new(6),
         )
     );
 }
