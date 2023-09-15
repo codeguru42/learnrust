9f6e6a0be87c0c61a7ff4993ab80f2d677706b31
Code-Apprentice <codeguru42@gmail.com>
2023-09-15 16:11:14 -0600

    DrunkardWalkArchitect::drunkard()

--- dungeoncrawl/src/map_builder/drunkard.rs
+++ dungeoncrawl/src/map_builder/drunkard.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 use super::MapArchitect;
 
+const STAGGER_DISTANCE: usize = 400;
 pub struct DrunkardWalkArchitect {}
 
 impl MapArchitect for DrunkardWalkArchitect {
@@ -14,5 +15,33 @@ impl MapArchitect for DrunkardWalkArchitect {
         };
 
         mb
+    }
+}
+
+impl DrunkardWalkArchitect {
+    fn drunkard(&mut self, start: &Point, rng: &mut RandomNumberGenerator, map: &mut Map) {
+        let mut drunkard_pos = start.clone();
+        let mut distance_staggered = 0;
+
+        loop {
+            let drunk_idx = map.point2d_to_index(drunkard_pos);
+            map.tiles[drunk_idx] = TileType::Floor;
+
+            match rng.range(0, 4) {
+                0 => drunkard_pos.x -= 1,
+                1 => drunkard_pos.x += 1,
+                2 => drunkard_pos.y -= 1,
+                _ => drunkard_pos.y += 1,
+            }
+
+            if !map.in_bounds(drunkard_pos) {
+                break;
+            }
+
+            distance_staggered += 1;
+            if distance_staggered > STAGGER_DISTANCE {
+                break;
+            }
+        }
     }
 }
